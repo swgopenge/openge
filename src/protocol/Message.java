5182224b55b30ce7b2e8d064b5fcb7f0c846b182
Ziggeh <ziggy@projectswg.com>
2014-08-02 20:58:54 +0200

    Refactored Message

--- src/protocol/Message.java
+++ src/protocol/Message.java
@@ -1,8 +1,8 @@
 package protocol;
 
-import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
 
 import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.buffer.SimpleBufferAllocator;
@@ -38,97 +38,34 @@ public abstract class Message {
 	public abstract IoBuffer	serialize();
 	
 	protected String getAsciiString(IoBuffer buffer) {
-		return getString(buffer, "US-ASCII");
+		return new String(buffer.array(), buffer.position(), buffer.order(ByteOrder.LITTLE_ENDIAN).getShort(), StandardCharsets.US_ASCII);
 	}
 	
 	protected String getUnicodeString(IoBuffer buffer) {
-		return getString(buffer, "UTF-16LE");
+		return new String(buffer.array(), buffer.position(), buffer.order(ByteOrder.LITTLE_ENDIAN).getInt(), StandardCharsets.UTF_16LE);
 	}
 	
 	protected byte[] getAsciiString(String string) {
-		return getString(string, "US-ASCII");
+		ByteBuffer result;
+		
+		result = ByteBuffer.allocate(2 + string.length()).order(ByteOrder.LITTLE_ENDIAN);
+		result.putShort((short) string.length());
+		result.put(string.getBytes(StandardCharsets.US_ASCII));
+		
+		return result.array();
 	}
 	
 	protected byte[] getUnicodeString(String string) {
-		return getString(string, "UTF-16LE");
-	}
-	
-	protected String getString(IoBuffer buffer, String charFormat) {
-		
-		String result;
-		
-		int length;
-		
-		if (charFormat == "UTF-16LE") length = buffer.order(ByteOrder.LITTLE_ENDIAN).getInt();
-		else length = buffer.order(ByteOrder.LITTLE_ENDIAN).getShort();
-		
-		int bufferPosition = buffer.position();
-		
-		try {
-			result = new String(buffer.array(), bufferPosition, length, charFormat);
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return "";
-		}
-		
-     	//TODO: Verify if character position is set correctly after reading a UTF-16 string
-		buffer.position(bufferPosition + length);
-		
-		return result;
-		
-	}
-	
-	private byte[] getString(String string, String charFormat) {
-		
 		ByteBuffer result;
-		int length = 2 + string.length();
 		
-		if (charFormat == "UTF-16LE") {
-			result = ByteBuffer.allocate(length * 2).order(ByteOrder.LITTLE_ENDIAN);
-			result.putInt(string.length());
-		} else {
-			result = ByteBuffer.allocate(length).order(ByteOrder.LITTLE_ENDIAN);
-			result.putShort((short) string.length());
-		}
-		
-		try {
-			result.put(string.getBytes(charFormat));
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return new byte[] {};
-		}
+		result = ByteBuffer.allocate((2 + string.length()) * 2).order(ByteOrder.LITTLE_ENDIAN);
+		result.putInt(string.length());
+		result.put(string.getBytes(StandardCharsets.UTF_16LE));
 		
 		return result.array();
-		
-	}
-
-	//FIXME: this is redundant and was also bugged. At some point, get rid of the redundant functions.
-	/**
-	 * Reads the next string in. It gets the length of the
-	 * string from the short/int at the beginning of it
-	 * @param bb The ByteBuffer to read from
-	 * @param charset Charset of the string
-	 * @return The read string. Returns "" if exception is thrown
-	 */
-	protected String getNextString(IoBuffer bb, String charset) {
-		try {
-			int length = 0;
-			if (charset.equals("US-ASCII") || charset.equals("UTF-8")) {
-				length = bb.getShort();
-			} else {
-				length = bb.getInt() * 2;
-			}
-			if (length > bb.remaining())
-				return "";
-			byte [] data = new byte[length];
-			bb.get(data);
-			return new String(data, charset);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return "";
 	}
 	
+	/*
 	/**
 	 * Reads the next array with specified size and returns it
 	 * @param bb The ByteBuffer to read from
@@ -150,11 +87,29 @@ public abstract class Message {
 	
 	
 	protected String getNextAsciiString(IoBuffer buffer) {
-		return getNextString(buffer, "US-ASCII");
+		short length = buffer.getShort();
+		
+		if (length > buffer.remaining())
+			return "";
+		
+		byte [] data = new byte[length];
+		buffer.get(data);
+		
+		return new String(data, StandardCharsets.US_ASCII);
+		
 	}
 	
 	protected String getNextUnicodeString(IoBuffer buffer) {
-		return getNextString(buffer, "UTF-16LE");
+		int length = buffer.getInt() * 2;
+		
+		if (length > buffer.remaining())
+			return "";
+		
+		byte [] data = new byte[length];
+		buffer.get(data);
+		
+		return new String(data, StandardCharsets.UTF_16LE);
+		
 	}
 	
 }
